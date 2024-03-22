67d39a717a5fb3ea0b6d1b72b37a6ffcacf7a2b3
SMPNayanajith <pasindusamarakoon500@gmail.com>
2024-03-22 13:50:54 +0530

    commit of register

--- bestGuru/src/Component/NavBar/NavBar.jsx
+++ bestGuru/src/Component/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@
 export default function NavBar() {
   return (
     <div className=" h-14  dark:bg-gray-700 ">
-      <h2 className="d-flex p-3.5 text-center text-4xl font-font-sans">Best <b>Guru</b>.LK</h2>
+      <h2 className="d-flex p-3.5 text-center text-4xl font-font-mono font-mono text-gray-900">Best <b className="text-white">Guru</b>.LK</h2>
 
     </div>
   )
--- bestGuru/src/Component/Register/Register.jsx
+++ bestGuru/src/Component/Register/Register.jsx
@@ -13,26 +13,26 @@ export default function Register() {
 <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
       
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-white dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1 className="text-xl font-bold c tracking-tight text-gray-900 md:text-2xl dark:text-black">
                   Create an account
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Full Name</label>
                       <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="abcd...." required=""></input>
                   </div>
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
                       <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""></input>
                   </div>
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
                       <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""></input>
                   </div>
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Confirm password</label>
                       <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""></input>
                   </div>
                   <div className="flex items-start">
@@ -40,12 +40,12 @@ export default function Register() {
                         <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
                       </div>
                       <div className="ml-3 text-sm">
-                        <label  className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                        <label  className="font-light text-gray-500 dark:text-gray-800">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                       </div>
                   </div>
                   <button type="submit" className="w-full text-white bg-green-500 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-green-800">Register</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <Link to='/' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                      Already have an account? <Link to='/' className="font-medium text-primary-600 hover:underline dark:text-gray-700">Login here</Link>
                       
                   </p>
               </form>
